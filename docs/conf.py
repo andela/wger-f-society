2afed01c19e7439a6833d4be051930d076379020
chadwalt <chadwalt04@gmail.com>
2017-10-04 13:11:43 +0300

    [Chore #151629853] Remove under-indentations

--- docs/conf.py
+++ docs/conf.py
@@ -200,7 +200,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     ('index', 'wgerWorkoutManager.tex', u'wger Workout Manager Documentation',
-    u'Roland Geider', 'manual'),
+     u'Roland Geider', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -243,9 +243,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'wgerWorkoutManager', u'wger Workout Manager Documentation',
-   u'Roland Geider', 'wgerWorkoutManager', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'wgerWorkoutManager', u'wger Workout Manager Documentation',
+     u'Roland Geider', 'wgerWorkoutManager', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
