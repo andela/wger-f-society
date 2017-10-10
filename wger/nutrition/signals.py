145ea28c2c7652cbf8593465038c713a15188784
Onen Julius <jonen54@gmail.com>
2017-10-10 18:58:13 +0300

    [Feature #151629856] Add multiple senders

--- wger/nutrition/signals.py
+++ wger/nutrition/signals.py
@@ -1,9 +1,13 @@
-from wger.nutrition.models import NutritionPlan
+from wger.nutrition.models import NutritionPlan,Meal,MealItem
 from django.db.models.signals import post_delete,post_save
 from django.dispatch import receiver
 from django.core.cache import cache
 
+signal_types=[post_delete,post_save]
 
-@receiver([post_delete,post_save], sender=NutritionPlan)
-def delete_plan(sender,instance,**kwargs):
-	cache.delete(instance.id)
+@receiver(signal_types, sender=NutritionPlan)
+@receiver(signal_types, sender=Meal)
+@receiver(signal_types, sender=MealItem)
+def delete_cache(sender,instance,**kwargs):
+	plan=instance.get_owner_object()
+	cache.delete(plan.id)
